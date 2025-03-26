4af0140d652906d8f2a6eb5cb27a0286fa5f9115
Edinei Cavalcanti <hi@edinei.dev>
2025-03-26 19:46:13 -0300

    Add Pedido and PedidoItem models and register them in admin
    Introduced new models, Pedido and PedidoItem, to represent orders and their items. Updated the admin interface to manage these new models along with Produto. Additionally, added string representations (__str__) for all models to improve readability.

--- nasa_store/admin.py
+++ nasa_store/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Produto
+from .models import Produto, Pedido, PedidoItem
 
 # Register your models here.
 
 admin.site.register(Produto)
+admin.site.register(Pedido)
+admin.site.register(PedidoItem)
--- nasa_store/models.py
+++ nasa_store/models.py
@@ -5,3 +5,23 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField(max_length=200)
     preco = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Produto: {self.nome} - {self.preco}'
+
+class Pedido(models.Model):
+    nome_cliente = models.CharField(max_length=200)
+    nome_vendedor = models.CharField(max_length=200)
+    pago = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Pedido: #{self.id} - {self.nome_cliente} - {self.nome_vendedor} - Pago? {self.pago}'
+
+class PedidoItem(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    quantidade = models.IntegerField()
+    preco_un = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f'Item do pedido: #{self.pedido.id} - {self.produto.nome} - {self.quantidade} x {self.preco_un}'
