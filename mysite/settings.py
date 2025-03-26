5d64bacc1b799fb058abdefeaf4d1da352344ab0
Edinei Cavalcanti <hi@edinei.dev>
2025-03-26 18:25:26 -0300

    Aula 2 pt 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'nasa_store.apps.NasaStoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ nasa_store/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NasaStoreConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'nasa_store'
