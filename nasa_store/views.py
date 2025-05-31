58495cf5d93967b885f7dc6ac0258d17eece7a40
Edinei Cavalcanti <hi@edinei.dev>
2025-05-31 19:02:53 -0300

    Initialize nasa_store app with models, migrations, templates, and views. Add Produto, Pedido, and PedidoItem models, create initial migration, and set up basic index view with template. Update `mysite/urls.py` to include nasa_store routes.

--- nasa_store/views.py
+++ nasa_store/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+from .models import Produto
+
+
+def index(request):
+    latest_question_list = Produto.objects.all()
+    template = loader.get_template("produto/index.html")
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
